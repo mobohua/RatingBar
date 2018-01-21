0ffacd4160435870819e768d72706c295965952c
boowa <mobohua@gmail.com>
2018-01-22 01:51:56 +0800

    update

--- library/src/main/java/com/boowa/ratingbar/RatingBar.java
+++ library/src/main/java/com/boowa/ratingbar/RatingBar.java
@@ -6,10 +6,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Shader;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Region;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -23,11 +24,10 @@ public class RatingBar extends View {
     private int mStarSize;
     private float mStarMark;
 
-    private Bitmap mStarFullBitmap;
-    private Bitmap mStarEmptyBitmap;
+    private Drawable mStarFullDrawable, mStarEmptyDrawable;
 
     private OnStarChangeListener mOnStarChangeListener;
-    private Paint mFullPaint, mEmptyPaint;
+
     private boolean mIntegerMark;
 
     private boolean mCanMove;
@@ -49,8 +49,8 @@ public class RatingBar extends View {
         mStarMargin = typedArray.getDimensionPixelSize(R.styleable.RatingBar_starMargin, 0);
         mStarSize = typedArray.getDimensionPixelSize(R.styleable.RatingBar_starSize, 20);
         mStarCount = typedArray.getInteger(R.styleable.RatingBar_starCount, 5);
-        mStarEmptyBitmap = drawableToBitmap(typedArray.getDrawable(R.styleable.RatingBar_starEmpty));
-        mStarFullBitmap = drawableToBitmap(typedArray.getDrawable(R.styleable.RatingBar_starFull));
+        mStarEmptyDrawable = typedArray.getDrawable(R.styleable.RatingBar_starEmpty);
+        mStarFullDrawable = typedArray.getDrawable(R.styleable.RatingBar_starFull);
 
         mIntegerMark = typedArray.getBoolean(R.styleable.RatingBar_integerMark, false);
         mStarMark = typedArray.getFloat(R.styleable.RatingBar_starMark, 0);
@@ -63,13 +63,6 @@ public class RatingBar extends View {
 
         typedArray.recycle();
 
-        mFullPaint = new Paint();
-        mFullPaint.setAntiAlias(true);
-        mFullPaint.setShader(new BitmapShader(mStarFullBitmap, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP));
-
-        mEmptyPaint = new Paint();
-        mEmptyPaint.setAntiAlias(true);
-        mEmptyPaint.setShader(new BitmapShader(mStarEmptyBitmap, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP));
 
     }
 
@@ -90,7 +83,7 @@ public class RatingBar extends View {
         }
         mStarMark = starMark;
         if (this.mOnStarChangeListener != null) {
-            this.mOnStarChangeListener.onStarChange(mStarMark);  //调用监听接口
+            this.mOnStarChangeListener.onStarChange(mStarMark);
         }
 
         invalidate();
@@ -117,32 +110,41 @@ public class RatingBar extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (mStarFullBitmap == null || mStarEmptyBitmap == null) {
+        if (mStarFullDrawable == null || mStarEmptyDrawable == null) {
             return;
         }
 
         for (int i = 0; i < mStarCount; i++) {
 
-            if (i != 0) {
-                canvas.translate(mStarMargin + mStarSize, 0);
-            }
-
 
             if (mStarMark - 1 >= i) {
-                canvas.drawRect(0, 0, mStarSize, mStarSize, mFullPaint);
-            } else {
-                if (mStarMark > i) {
-                    canvas.drawRect(0, 0, (mStarMark % 1) * mStarSize, mStarSize, mFullPaint);
 
-                    canvas.drawRect((mStarMark % 1) * mStarSize, 0, mStarSize, mStarSize, mEmptyPaint);
-                } else {
-                    canvas.drawRect(0, 0, mStarSize, mStarSize, mEmptyPaint);
-                }
+                mStarFullDrawable.setBounds(i * (mStarMargin + mStarSize), 0, i * (mStarMargin + mStarSize) + mStarSize, mStarSize);
+                mStarFullDrawable.draw(canvas);
+
+            } else if (mStarMark > i) {
+
+                mStarEmptyDrawable.setBounds(i * (mStarMargin + mStarSize), 0, i * (mStarMargin + mStarSize) + mStarSize, mStarSize);
+                mStarEmptyDrawable.draw(canvas);
+
+                canvas.save();
+                canvas.clipRect(i * (mStarMargin + mStarSize), 0, (int) (i * (mStarMargin + mStarSize) + (mStarMark % 1) * mStarSize), mStarSize);
+
+                mStarFullDrawable.setBounds(i * (mStarMargin + mStarSize), 0, i * (mStarMargin + mStarSize) + mStarSize, mStarSize);
+                mStarFullDrawable.draw(canvas);
+
+                canvas.restore();
+
+            } else {
+
+                mStarEmptyDrawable.setBounds(i * (mStarMargin + mStarSize), 0, i * (mStarMargin + mStarSize) + mStarSize, mStarSize);
+                mStarEmptyDrawable.draw(canvas);
 
             }
 
 
         }
+
 
     }
 
@@ -178,31 +180,7 @@ public class RatingBar extends View {
         return super.onTouchEvent(event);
     }
 
-    private Bitmap drawableToBitmap(Drawable drawable) {
-        if (drawable == null) return null;
-        Bitmap bitmap = Bitmap.createBitmap(mStarSize, mStarSize, Bitmap.Config.ARGB_4444);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, mStarSize, mStarSize);
-        drawable.draw(canvas);
-        return bitmap;
-    }
-
-
-    @Override
-    protected void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-
-        if (mStarFullBitmap != null && !mStarFullBitmap.isRecycled()) {
-            mStarFullBitmap.recycle();
-            mStarFullBitmap = null;
-        }
-
-        if (mStarEmptyBitmap != null && mStarEmptyBitmap.isRecycled()) {
-            mStarEmptyBitmap.recycle();
-            mStarEmptyBitmap = null;
-        }
-
-    }
 
 }
+
 
